ed99540ec94616018067336eeea4872f1fcabfd3
Lois Garcion <115794790+LoisGarcion@users.noreply.github.com>
2025-10-17 09:38:41 -0400

    Merge dev into eq07 (#11)
    * changes to deploy.sh files and new directory "lionel" that we can use to start the containers from the public images on the dockerhub. BE CAREFUL you need to create docker network first
    
    * Removed /microservice from FE api calls
    Fixed the testapi-service docker compose
    
    ---------
    
    Co-authored-by: loisg-simwell <lgarcion@simwell.io>

--- selenium-test-Service/build.sh
+++ selenium-test-Service/build.sh
@@ -6,19 +6,19 @@ PASSWORD=$DOKCER_PASWORD
 REGISTRY=$REGISTRY_BASE
 
 docker compose -f docker-compose-build.yml build backend
-
-docker tag lionel-test-team1-backend ${REGISTRY}/lionel-test-team1-backend:latest
-docker tag lionel-test-team1-frontend ${REGISTRY}/lionel-test-team1-frontend:latest
-docker tag lionel-test-team1-testapi ${REGISTRY}/lionel-test-team1-testapi:latest
-docker tag lionel-test-team1-testui ${REGISTRY}/lionel-test-team1-testui:latest
-docker tag lionel-test-team1-testui-team4-server ${REGISTRY}/lionel-test-team1-testui-team4-server:latest
-docker tag lionel-test-team1-testui-team4-client ${REGISTRY}/lionel-test-team1-testui-team4-client:latest
+docker compose -f docker-compose-build.yml build frontend
+docker compose -f docker-compose-build.yml build selenium
+docker compose -f docker-compose-build.yml build testapi
+docker compose -f docker-compose-build.yml build testui
+docker compose -f docker-compose-build.yml build testuiteam4-server
+docker compose -f docker-compose-build.yml build testuiteam4-client
 
 echo '>>>> PUSH Image API_GATEWAY_VERSION : ' $API_GATEWAY_VERSION
 docker login $REGISTRY_URL -u $USERNAME -p $PASSWORD
-docker push ${REGISTRY}/lionel-test-team1-backend:latest
-docker push ${REGISTRY}/lionel-test-team1-frontend:latest
-docker push ${REGISTRY}/lionel-test-team1-testapi:latest
-docker push ${REGISTRY}/lionel-test-team1-testui:latest
-docker push ${REGISTRY}/lionel-test-team1-testui-team4-server:latest
-docker push ${REGISTRY}/lionel-test-team1-testui-team4-client:latest
+docker push ${REGISTRY}/lionel-test-team1-backend:${BACKEND_VERSION}
+docker push ${REGISTRY}/lionel-test-team1-frontend:${FRONTEND_VERSION}
+docker push ${REGISTRY}/lionel-test-team1-selenium:${SELENIUM_VERSION}
+docker push ${REGISTRY}/lionel-test-team1-testapi:${TEST_API_VERSION}
+docker push ${REGISTRY}/lionel-test-team1-testui:${TEST_UI_VERSION}
+docker push ${REGISTRY}/lionel-test-team1-testui-team4-server:${TEST_TEAM4_SERVER_VERSION}
+docker push ${REGISTRY}/lionel-test-team1-testui-team4-client:${TEST_TEAM4_CLIENT_VERSION}
